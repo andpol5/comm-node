ac02c417002f3da4f4eeb35d0bf0da5a28748ee5
Andrei Polzounov <and.pol5@gmail.com>
2016-12-09 15:28:02 +0800

    boost::asio working and simple app for accessing daytime server

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,47 @@
-/*
- * DESCRIPTION:
- *  Simple program to shuffle a deck of cards in the following manner:
- *    1. Take the top card off the deck and set it on the table
- *    2. Take the next card off the top and put it on the bottom of the deck in your hand.
- *    3. Continue steps 1 and 2 until all cards are on the table. This is a round.
- *    4. Pick up the deck from the table and repeat steps 1-3 until the deck is in the original order.
- *
- * INPUT: number_of_cards_in_deck
- * OUTPUT: number_of_shuffle_rounds (until the deck is in the original order)
- */
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
-#include <string>
-#include <unistd.h>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
 
-#include "CommsNode.h"
+using boost::asio::ip::tcp;
 
-void print_help_and_exit(const char* argv0);
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: client <host>" << std::endl;
+      return 1;
+    }
 
-int main(int argc, char **argv) {
+    boost::asio::io_service io_service;
 
-	std::string filename;
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query(argv[1], "daytime");
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
 
-	// Parse arguments using GNU getopt
-	int opt;
-	int n = -1;
-	while ((opt = getopt(argc, argv, "hn:f:")) != -1) {
-		switch (opt) {
-		case 'h':
-			print_help_and_exit(argv[0]);
-			break;
-		case 'n':
-			n = atoi(optarg);
-			break;
-		case 'f':
-			filename = std::string(optarg);
-			break;
-		default:
-			print_help_and_exit(argv[0]);
-			return 0;
-		}
-	}
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoint_iterator);
 
-	// Check if -n was entered and parsed properly
-	if(n == -1){
-		print_help_and_exit(argv[0]);
-	}
-	else if(n < 0 || n == 0){
-		printf("Deck size is 0, negative or could not be parsed, exiting program\n");
-		return 0;
-	}
+    for (;;)
+    {
+      boost::array<char, 128> buf;
+      boost::system::error_code error;
 
-	// Get the # of turns needed for shuffling
-	printf("Number of turns needed for shuffling %d cards is %d\n", n, 2);
-	return 0;
-}
+      size_t len = socket.read_some(boost::asio::buffer(buf), error);
 
-void print_help_and_exit(const char* argv0){
-	printf("Usage: %s [-h] [-n] NUM_CARDS [-o] FILE\n", argv0);
-	printf("       -n NUM_CARDS  (required) number of cards to shuffle\n");
-	printf("       -f FILE       (optional) output csv file\n");
-	printf("       -h            print this help menu and exit\n");
-	exit(0);
+      if (error == boost::asio::error::eof)
+        break; // Connection closed cleanly by peer.
+      else if (error)
+        throw boost::system::system_error(error); // Some other error.
+
+      std::cout.write(buf.data(), len);
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
 }
